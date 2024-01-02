cd2a4fa3d9bbfb82fda550b454e2674c4fc5d619
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-02 13:36:48 -0400

    ajustes

--- Models/Formacao.cs
+++ Models/Formacao.cs
@@ -6,8 +6,8 @@ namespace Curriculo_App_Back.Models
     {
         [Key]
         public int IdFormacao { get; set; }
-        public string Instituicao { get; set; }
-        public string Curso { get; set; }
+        public string? Instituicao { get; set; }
+        public string? Curso { get; set; }
         public  bool CursoFinalizado { get; set; }
         public DateTime DataInicio { get; set; }
         public  DateTime DataTermino { get; set; }
